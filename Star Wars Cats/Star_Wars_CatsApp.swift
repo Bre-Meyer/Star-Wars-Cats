e0717e5d77d478e6393fb3e980201f237da7def2
Bre Meyer <bmeyer@myocv.com>
2021-06-22 02:37:25 -0500

    Initial Commit

---
+++ Star Wars Cats/Star_Wars_CatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Star_Wars_CatsApp.swift
+//  Star Wars Cats
+//
+//  Created by Bre Meyer on 6/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Star_Wars_CatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
